26a1b6d51e54abffa1b34d209a57b9e465a214e9
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-02-02 20:05:38 +0100

    Add function that displays gameBoard element on the page

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ const players = () => {
 
 const gameBoard = (() => {
     'use strict';
-
+    const gameContainer = document.querySelector('.game-container')
     const gameBoard = ['X', 'O', 'X', 'X', 'O', 'O', 'O', 'X', 'X'];
     //code to display array data on the page
+    function displayArray() {
+        for (let i = 0; i < gameBoard.length; i++) {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('sign');
+            newDiv.innerHTML = gameBoard[i];
+            gameContainer.appendChild(newDiv);
+        }
+    }
+    return {
+        displayArray
+    };
 
 })();
+
+gameBoard.displayArray();
