7f602d6d06aa879b98bb439721190f28efe3e7e1
Marek-Kosmowski <kosmowski.marek@gmail.com>
2023-02-03 17:17:48 +0100

    Add winning conditions in scripts.js

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ const Player = (sign) => {
     };
 }
 
+const winingConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+
 
 const gameBoard = (() => {
     'use strict';
